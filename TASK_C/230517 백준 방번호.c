16cc6704c9835f367f0ddefdc2070ea8cc6b0a02
DJMAXJOAA <cpsn6237@gmail.com>
2023-05-18 00:20:12 +0900

    a

--- TASK_C/230517 백준 방번호.c
+++ TASK_C/230517 백준 방번호.c
@@ -3,28 +3,82 @@
 #include <stdlib.h>
 #include <string.h>
 
+//int main()
+//{
+//	char n[10] = { 0 };
+//	scanf("%s", &n);
+//	int n_count[10] = { 0 };
+//	
+//	int max = 0;
+//
+//	for (int i = 0; i < strlen(n); i++)
+//	{
+//		int temp = n[i] - '0';
+//		n_count[temp]++;
+//	}
+//
+//	n_count[6] = (n_count[6] += n_count[9] + 1) / 2;
+//
+//	for (int i = 0; i < 8; i++)
+//	{
+//		if (n_count[i] > max)
+//		{
+//			max = n_count[i];
+//		}
+//	}
+//
+//	printf("%d", max);
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main()
 {
-	char n[10] = { 0 };
-	scanf("%s", &n);
+	char n[1000] = { 0 };
+	scanf("%s", n);
 	int n_count[10] = { 0 };
-	
+
 	int max = 0;
 
 	for (int i = 0; i < strlen(n); i++)
 	{
-		int temp = n[i] - '0';
-		n_count[temp]++;
+		for (int j = 0; j < 10; j++)
+		{
+			if (n[i] - '0' == j)
+			{
+				n_count[j]++;
+				break;
+			}
+		}
 	}
 
-	n_count[6] = (n_count[6] += n_count[9] + 1) / 2;
 
-	for (int i = 0; i < 8; i++)
+	while (n_count[6] - n_count[9] != 1 && n_count[9] - n_count[6] != 1)
 	{
-		if (n_count[i] > max)
+		if (n_count[6] == n_count[9])
 		{
-			max = n_count[i];
+			break;
 		}
+		if (n_count[6] > n_count[9] && n_count[6] >= 2)
+		{
+			n_count[6]--;
+			n_count[9]++;
+		}
+
+		if (n_count[9] > n_count[6] && n_count[9] >= 2)
+		{
+			n_count[9]--;
+			n_count[6]++;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (n_count[i] > max) max = n_count[i]; 
 	}
 
 	printf("%d", max);
--- TASK_C/230517 재귀함수 백준.c
+++ TASK_C/230517 재귀함수 백준.c
@@ -3,53 +3,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-char recursive(int n);
-
-int count = 0;
-int x = 0;
-int main()
+/* 재귀함수로 언더바 출력 */
+int printstr(int num)
 {
-	int n = 2;
-	printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
-	recursive(n);
-	printf("라고 답변하였지.");
-	return 0;
+	if (num <= 0) return 0;
+	printf("____");
+	return printstr(num - 1);
 }
 
-char recursive(int n)
+/* 재귀함수, 0부터 s까지 오름 */
+int recursive(int num, int s)
 {
-	if (count == n && x == 0)
-	{
-		for (int i = 0; i < count; i++) printf("____");
-		printf("라고 답변하였지.\n");
-		count--;
-		if (count == 0)
-		{
-			return 0;
-		}
-		return recursive(n - 1);
-	}
-	for (int i = 0; i < count; i++) printf("____");
+	printstr(num);
 	printf("\"재귀함수가 뭔가요?\"\n");
-	if (count == n)
+	if (num >= s)	// 재귀함수 멈추는 트리거
 	{
-		for (int i = 0; i < count; i++) printf("____");
+		printstr(num);
 		printf("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
-		count--;
-		x++;
-		return recursive(n - 1);
+		printstr(num);
+		printf("라고 답변하였지.\n");
+		return 1;	// 다시 돌아가기
 	}
-	else
-	{
-		for (int i = 0; i < count; i++) printf("____");
-		printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+	printstr(num);
+	printf("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+	printstr(num);
+	printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+	printstr(num);
+	printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+	return recursive(num + 1, s) + printstr(num) + printf("라고 답변하였지.\n");
+}
 
-		for (int i = 0; i < count; i++) printf("____");
-		printf("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
-
-		for (int i = 0; i < count; i++) printf("____");
-		printf("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
-	}
-	count++;
-	return recursive(n);
+int main()
+{
+	
+	int num = 0;
+	int s;
+	scanf("%d", &s);
+	printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+	recursive(num, s);
+	return 0;
 }
